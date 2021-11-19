07c176ea39642abdb954eda0a579fd8f054dfddd
chinomso <posh.bc@gmail.com>
2021-11-19 23:18:40 +0100

    Add singleton implementations

---
+++ src/main/java/com/looseboxes/ratelimiter/web/javaee/RateExceededHandlerImpl.java
@@ -0,0 +1,9 @@
+package com.looseboxes.ratelimiter.web.javaee;
+
+import com.looseboxes.ratelimiter.RateExceededExceptionThrower;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class RateExceededHandlerImpl extends RateExceededExceptionThrower {
+}
