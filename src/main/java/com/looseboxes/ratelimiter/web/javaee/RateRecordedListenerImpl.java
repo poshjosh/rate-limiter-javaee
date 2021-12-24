9df2504705f609eb62f5b023f3e9a31aa942d768
chinomso <posh.bc@gmail.com>
2021-12-24 20:19:56 +0100

    Rename

--- src/main/java/com/looseboxes/ratelimiter/web/javaee/RateRecordedListenerImpl.java
+++ src/main/java/com/looseboxes/ratelimiter/web/javaee/RateRecordedListenerImpl.java
@@ -5,5 +5,5 @@ import com.looseboxes.ratelimiter.RateExceededExceptionThrower;
 import javax.inject.Singleton;
 
 @Singleton
-public class RateExceededListenerImpl extends RateExceededExceptionThrower {
+public class RateRecordedListenerImpl extends RateExceededExceptionThrower {
 }
